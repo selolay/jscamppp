129a29363cd0ae1ef29e11f7626eb8a865f7fad1
selolay <selinayxaydn@gmail.com>
2021-10-19 00:33:18 +0300

    proje yüklendi.

---
+++ src/app.js
@@ -0,0 +1,23 @@
+console.log("merhaba kodlama.io")
+
+let dolarBugun = 9.30
+
+let dolarDun 
+dolarDun = 9.20
+console.log(dolarDun)
+
+const euroDun = 11.2
+console.log(euroDun)
+
+
+let konutKredileri= ["konut kredisi","emlak konur kredisi","komu konut kredisi","rivrivr"]
+
+console.log("<ul>")
+
+for(let i = 0 ; i<konutKredileri.length ; i++){
+    console.log("<li>" +konutKredileri[i] + "</li>")
+}
+
+console.log("</ul>")
+
+console.log(konutKredileri)
